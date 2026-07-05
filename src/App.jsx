36d48f21a8cb90959078958dba65d6a7eb81ec88
Tobiloba <onabanjt@gmail.com>
2026-07-05 19:52:44 +0100

    fix: correct routing and fix deploy issues

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Port from "./Port";
 
-import Navbar from "./components/NewNavbar";
-
 function App() {
   return (
-    // <Router>
-    //   <Routes>
-    //     <Route path="/" element={<Port />} />
-    //   </Routes>
-    // </Router>
-    <Navbar />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Port />} />
+      </Routes>
+    </Router>
   );
 }
 
